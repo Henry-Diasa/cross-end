b57361ebcbdd5fafe91cb5352eaa24cb3ca92459
fuqiang1 <fuqiang1@sensetime,com>
2021-10-21 17:14:37 +0800

    uni-demo

---
+++ uni-demo/pages/feeds/feeds.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		<text>feeds23</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
