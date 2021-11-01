549f78834e82cc41fe508e156032a0199e642f33
fuqiang1 <fuqiang1@sensetime,com>
2021-11-01 23:47:49 +0800

    mini-demo

---
+++ mini-demo/pages/posts/posts.js
@@ -0,0 +1,76 @@
+// pages/posts/posts.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    a: '2020LPL季后赛'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    const content = {
+      title: "2020LPL夏季赛",
+      content: "2020LPL夏季赛2020LPL夏季赛2020LPL夏季赛2020LPL夏季赛2020LPL夏季赛2020LPL夏季赛2020LPL夏季赛",
+      imgSrc: "/images/swiper1.webp",
+      reading: 120,
+      collection: 98,
+      dateTime: '2小时',
+      headImgSrc: "/images/avatar.jpg",
+      author: '猫',
+    }
+    this.setData(content)
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    console.log('onPullDownRefresh')
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
